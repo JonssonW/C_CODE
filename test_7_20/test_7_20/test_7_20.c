5975a72ecd6a9196c9c9d1ebc0dae610f7070cec
JonssonW <16666183321@163.COM>
2022-07-20 22:42:19 +0800

    初识数据类型、常量和变量

---
+++ test_7_20/test_7_20/test_7_20.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h> 
+//printf库函数的使用需要引用头文件stdio.h
+//一个工程中main函数有且仅有一个，main函数是程序的入口
+//int age = 100; //全局变量
+int main() //int 函数的返回类型，main 函数名
+{
+	//int a = 0;
+	//int b = 0;
+	//int sum = 0;
+	//scanf("%d %d", &a, &b);
+	//sum = a + b;
+	//printf ("sum = %d\n",sum);	
+	
+
+	//字符类型大小
+	//bit-比特位  byte-字节=8bit
+	//sizeof的单位是-字节 byte
+	
+	/*printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short)); 
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));*/
+	
+	//int age = 20; //定义变量
+	//float weight = 75.6; //在{}内是局部变量，在{}外是全局变量 当全局变量和局部变量重名时，局部变量优先于全局变量
+	//char ch = 'a';
+	//age = age++;
+	//printf("age=%d\n", age);
+
+
+	return 0;
+}//函数体
