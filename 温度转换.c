0cb7f12dbc3975fe0625a98bb682ec84bc9ed665
JonssonW <109575434+JonssonW@users.noreply.github.com>
2022-08-14 01:20:06 +0800

    浮点数的公式计算与输出

---
+++ 温度转换.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	double f = 0;
+	double c = 0;
+	scanf("%lf", &f);
+	c = 5.0 / 9 * (f - 32);
+	//整数相整除，不足1得0,0与其他相乘均为0
+	//得到结果为0.000，解决需要把5或9改为5.0或9.0
+	printf("%.3f", c);
+	return 0;
+}
