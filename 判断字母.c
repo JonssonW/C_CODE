6313e56f50f9b9b257093ebca41252fe771ec449
JonssonW <109575434+JonssonW@users.noreply.github.com>
2022-08-16 21:04:15 +0800

    getchar()的缓冲清除

---
+++ 判断字母.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	char ch;
+	while ((ch=getchar()) != EOF)  //(scanf("%c", &ch) != EOF) 
+	{
+		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+		{
+			printf("YES");
+			getchar();
+		}
+		else
+		{
+			printf("NO");
+			getchar();
+		}
+	}
+	return 0;
+}
